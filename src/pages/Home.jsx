b651ff8cbff52318f3ece8e7dd478188d5578396
Jhorene Arenas <jhorene.arenas@wundermanthompson.com>
2023-12-14 12:57:02 +0000

    Initial imports for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,13 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import {
+  headContainerAnimtion,
+  headtContextAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+
 function Home() {
   return <div>Home</div>;
 }
