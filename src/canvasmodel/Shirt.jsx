edb51bcfb9cf0dfc8522ce3211aa11b6033b8026
Jhorene Arenas <jhorene.arenas@wundermanthompson.com>
2023-12-14 20:09:34 +0000

    Build shirt model

--- src/canvasmodel/Shirt.jsx
+++ src/canvasmodel/Shirt.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import { useGLTF, useTexture } from "@react-three/drei";
 
 function Shirt() {
-  return <div>Shirt</div>;
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
+
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+
+  return (
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      ></mesh>
+    </group>
+  );
 }
 
 export default Shirt;
--- src/canvasmodel/index.jsx
+++ src/canvasmodel/index.jsx
@@ -3,9 +3,22 @@ import { Canvas } from "@react-three/fiber";
 import Shirt from "./Shirt";
 import Backdrop from "./Backdrop";
 import CameraRig from "./CameraRig";
+import { Center, Environment } from "@react-three/drei";
 
 function CanvasModel() {
-  return <div>CanvasModel</div>;
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      {/* <CameraRig> */}
+      {/* <Backdrop /> */}
+      <Center>
+        <Shirt />
+      </Center>
+      {/* </CameraRig> */}
+    </Canvas>
+  );
 }
 
 export default CanvasModel;
