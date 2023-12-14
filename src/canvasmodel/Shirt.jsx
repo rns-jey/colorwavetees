060c03cd1550e6a30f28b9cf2f2c49844a72e171
Jhorene Arenas <jhorene.arenas@wundermanthompson.com>
2023-12-14 19:46:04 +0000

    Import canvas components

---
+++ src/canvasmodel/Shirt.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Shirt() {
+  return <div>Shirt</div>;
+}
+
+export default Shirt;
--- src/canvasmodel/index.jsx
+++ src/canvasmodel/index.jsx
@@ -1,5 +1,9 @@
 import { Canvas } from "@react-three/fiber";
 
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+
 function CanvasModel() {
   return <div>CanvasModel</div>;
 }
