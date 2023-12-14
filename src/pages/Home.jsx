c634e881fd3e8ef3f4ec80a835d4a836d6db477d
Jhorene Arenas <jhorene.arenas@wundermanthompson.com>
2023-12-14 15:13:09 +0000

    Import custom button ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 
 import state from "../store";
+import { CustomButton } from "../components";
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -38,6 +39,13 @@ function Home() {
                 customization tool. <strong>Unleash your imagimation</strong>{" "}
                 and define your own style
               </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
           </motion.div>
         </motion.section>
