0739b18140657242ac3f93ab01c1eaa0be9b5cd2
Jhorene Arenas <jhorene.arenas@wundermanthompson.com>
2023-12-14 13:24:56 +0000

    Build animated intro text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,6 +29,16 @@ function Home() {
                 LET&apos;S <br className="x1:block hidden" /> DO IT.
               </h1>
             </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create your unique and exclusive shirt with our brand-new 3D
+                customization tool. <strong>Unleash your imagimation</strong>{" "}
+                and define your own style
+              </p>
+            </motion.div>
           </motion.div>
         </motion.section>
       )}
