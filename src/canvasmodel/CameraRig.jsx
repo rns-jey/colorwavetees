c5de439ee75491e02f7a122dcb550b1070973714
Jhorene Arenas <jhorene.arenas@wundermanthompson.com>
2023-12-14 20:28:58 +0000

    Build camera position and responsiveness

--- src/canvasmodel/CameraRig.jsx
+++ src/canvasmodel/CameraRig.jsx
@@ -9,6 +9,20 @@ function CameraRig({ children }) {
   const snap = useSnapshot(state);
 
   useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth >= 600;
+
+    let targetPosition = [-0.4, 0, 2];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
     easing.dampE(
       group.current.rotation,
       [state.pointer.y / 10, -state.pointer.x / 5, 0],
